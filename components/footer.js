f6f8ca7dd185f6fd8c3f0a64205a7e639efd9a12
Mario Vidal <mariojose9748@hotmail.com>
2021-04-03 20:29:54 -0600

    Media query para móviles

--- components/footer.js
+++ components/footer.js
@@ -3,12 +3,12 @@ import styles from '../styles/Home.module.scss'
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <div className={styles.logo}>
-                Portafolio
-            </div>
-
             <div className={styles.info}>
                 Creado por: Mario Vidal
+            </div>
+
+            <div className={styles.logo}>
+                Portafolio
             </div>
         </footer>
     )
