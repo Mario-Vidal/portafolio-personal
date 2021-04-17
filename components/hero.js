a32f9d0072e3d000a582a992d90ea4d1cdde0264
Mario Vidal <mariojose9748@hotmail.com>
2021-04-17 00:47:14 -0500

    Ajustes del navbar en móviles

--- components/hero.js
+++ components/hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <div className={styles.intro}>
-                {/* <h2>Hola Mundo</h2> */}
+
             </div>
         </div>
     )
